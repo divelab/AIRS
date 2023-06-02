5bbdc07aa09d13f736d1f88dccbc874b02519950
kruskallin <kruskallin@tamu.edu>
2023-06-02 01:53:45 -0500

    potnet

--- OpenMat/PotNet/main.py
+++ OpenMat/PotNet/main.py
@@ -7,6 +7,7 @@ import argparse
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Training configuration of PotNet')
     parser.add_argument('--config', type=str, help='Path to YAML configuration file')
+    parser.add_argument('--output_dir', type=str, default='output', help='Path to the output')
     parser.add_argument('--data_root', type=str, default=None, help='Path to the data')
     parser.add_argument('--checkpoint', type=str, default=None, help='Path to the checkpoint')
     parser.add_argument('--testing', action='store_true', help='Evaluation phase')
@@ -18,4 +19,5 @@ if __name__ == '__main__':
     with open(args.config, 'r') as file:
         data = yaml.safe_load(file)
 
+    data["output_dir"] = args.output_dir
     train_prop_model(data, data_root=args.data_root, checkpoint=args.checkpoint, testing=args.testing)
