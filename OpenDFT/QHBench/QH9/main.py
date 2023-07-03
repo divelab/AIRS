eb2ad06b031502918368e7986fc7d28126af30ab
Haiyang Yu <1161283769@qq.com>
2023-07-03 03:18:18 -0700

    Update main.py
    OpenDFT: QHBench: fix the loss function problem

--- OpenDFT/QHBench/QH9/main.py
+++ OpenDFT/QHBench/QH9/main.py
@@ -6,6 +6,7 @@ import logging
 from models import QHNet
 from torchvision.transforms import Compose
 from torch_geometric.loader import DataLoader
+from torch_scatter import scatter
 
 from datasets import QH9Stable, QH9Dynamic
 from torch_ema import ExponentialMovingAverage
@@ -24,21 +25,28 @@ def recorder_pos(data):
 def criterion(outputs, target, loss_weights):
     error_dict = {}
     keys = loss_weights.keys()
-    # the diagonal and non-diagonal should be considered with the mask
     try:
         for key in keys:
+            row = target.edge_index[0]
+            edge_batch = target.batch[row]
             diff_diagonal = outputs[f'{key}_diagonal_blocks']-target[f'diagonal_{key}']
-            mse_diagonal  = torch.sum(diff_diagonal**2 * target[f"diagonal_{key}_mask"])
-            mae_diagonal  = torch.sum(torch.abs(diff_diagonal) * target[f"diagonal_{key}_mask"])
-            count_sum_diagonal =  torch.sum(target[f"diagonal_{key}_mask"])
+            mse_diagonal  = torch.sum(diff_diagonal**2 * target[f"diagonal_{key}_mask"], dim=[1, 2])
+            mae_diagonal  = torch.sum(torch.abs(diff_diagonal) * target[f"diagonal_{key}_mask"], dim=[1, 2])
+            count_sum_diagonal =  torch.sum(target[f"diagonal_{key}_mask"], dim=[1, 2])
+            mse_diagonal = scatter_sum(mse_diagonal, target.batch)
+            mae_diagonal = scatter_sum(mae_diagonal, target.batch)
+            count_sum_diagonal = scatter_sum(count_sum_diagonal, target.batch)
 
             diff_non_diagonal = outputs[f'{key}_non_diagonal_blocks']-target[f'non_diagonal_{key}']
-            mse_non_diagonal  = torch.sum(diff_non_diagonal**2 * target[f"non_diagonal_{key}_mask"])
-            mae_non_diagonal  = torch.sum(torch.abs(diff_non_diagonal) * target[f"non_diagonal_{key}_mask"])
-            count_sum_non_diagonal =  torch.sum(target[f"non_diagonal_{key}_mask"])
+            mse_non_diagonal  = torch.sum(diff_non_diagonal**2 * target[f"non_diagonal_{key}_mask"], dim=[1, 2])
+            mae_non_diagonal  = torch.sum(torch.abs(diff_non_diagonal) * target[f"non_diagonal_{key}_mask"], dim=[1, 2])
+            count_sum_non_diagonal =  torch.sum(target[f"non_diagonal_{key}_mask"], dim=[1, 2])
+            mse_non_diagonal = scatter_sum(mse_non_diagonal, edge_batch)
+            mae_non_diagonal = scatter_sum(mae_non_diagonal, edge_batch)
+            count_sum_non_diagonal = scatter_sum(count_sum_non_diagonal, edge_batch)
 
-            mae = (mae_diagonal + mae_non_diagonal) / (count_sum_diagonal + count_sum_non_diagonal)
-            mse = (mse_diagonal + mse_non_diagonal) / (count_sum_diagonal + count_sum_non_diagonal)
+            mae = ((mae_diagonal + mae_non_diagonal) / (count_sum_diagonal + count_sum_non_diagonal)).mean()
+            mse = ((mse_diagonal + mse_non_diagonal) / (count_sum_diagonal + count_sum_non_diagonal)).mean()
 
             error_dict[key+'_mae']  = mae
             error_dict[key+'_rmse'] = torch.sqrt(mse)
