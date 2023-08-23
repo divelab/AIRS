2929b3292efb9d824a2682b4486fca0aceb584cb
Haiyang Yu <1161283769@qq.com>
2023-08-23 16:42:21 -0500

    Update install.sh

--- OpenDFT/QHBench/QH9/install.sh
+++ OpenDFT/QHBench/QH9/install.sh
@@ -1,7 +1,6 @@
 conda create -y -n QHBench python=3.8
 source activate QHBench
-CUDA="cu102"
-conda install -y pytorch==1.6.0 torchvision==0.7.0 cudatoolkit=10.2 -c pytorch
+conda install pytorch==1.11.0 torchvision==0.12.0 torchaudio==0.11.0 cudatoolkit=11.3 -c pytorch
 pip install scipy
 conda install pyg -c pyg
 pip install tqdm hydra-core pyscf rdkit transformers torch_ema e3nn
