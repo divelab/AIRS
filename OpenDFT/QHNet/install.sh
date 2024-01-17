4b5778e88ee8c1916904d9c1f3f0050d373b7ade
Haiyang Yu <1161283769@qq.com>
2024-01-17 16:00:26 -0600

    Update install.sh

--- OpenDFT/QHNet/install.sh
+++ OpenDFT/QHNet/install.sh
@@ -3,5 +3,5 @@ source activate QHNet
 CUDA="cu102"
 conda install -y pytorch==1.6.0 torchvision==0.7.0 cudatoolkit=10.2 -c pytorch
 pip install scipy
-conda install pyg -c pyg
+conda install pyg==2.2.0 -c pyg
 pip install tqdm hydra-core pyscf rdkit transformers torch_ema e3nn
